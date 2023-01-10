e6559aa59b8417de7c47e5b8bed378c4ea7c567e
TelTRiNoZ <title34718@gmail.com>
2023-01-10 16:16:35 +0700

    lab13_1.cpp complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int n){
+    
+    if(n==1){
+        return 1;
+    }
+    else if (n==2)
+    {
+        return 1;
+    }
+    else if (n==0)
+    {
+        return 0;
+    }
+    else{
+        return fibonacci(n-2)+fibonacci(n-1);
+    }
 }
